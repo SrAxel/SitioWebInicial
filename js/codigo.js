1db30a0ee1b6f722f9922baa77cf53be60ba7798
SrAxel <thesense@outlook.es>
2022-06-22 22:34:46 -0300

    prueba 1 correccion de errores

---
+++ js/codigo.js
@@ -0,0 +1,8 @@
+function cargarAyuda(){
+var h3=document.getElementById("h3Ayuda");
+h3.innerHTML="Ingrese con MAYUSCULAS su nombre";
+}
+function cargarAyuda2(){
+    var h3=document.getElementById("h3Ayuda");
+    h3.innerHTML="Ingrese con MAYUSCULAS su apeliido";
+    }
